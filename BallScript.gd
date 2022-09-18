3ef963e4afea8aabc51900e5944e6078cb267c3a
Pengalu <pengalu99@gmail.com>
2022-09-17 23:40:21 -0700

    Comments and fixed shadow draw distance

--- BallScript.gd
+++ BallScript.gd
@@ -11,7 +11,7 @@ var radius=0
 var drawPosition = null
 var drawPositionTable = null
 var originalPosition = null
-func getCurrentCamera2D():
+func getCurrentCamera2D(): #Returns whatever camera is being used right now since godot cannot use its native function for this on 2d cameras.
 	var viewport = get_viewport()
 	if not viewport:
 		return null
@@ -24,54 +24,43 @@ func getCurrentCamera2D():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	rng.randomize()
-	z = rng.randf_range(2,120)
+	z = rng.randf_range(2,120) #set a random depth position 
 	pass # Replace with function body.
 
 	
-func _draw():
-	if radius == 0:
-		radius = rng.randf_range(25.0, 100.0)
-		$Sprite.modulate = Color(rng.randf(),rng.randf(),rng.randf())
-		originalPosition = position
-		#z = rng.randf_range(-500.0,500.0)
-	#print(position)
-	drawPositionTable = WorldToScreen.wp_to_sp(originalPosition,z)
-	drawPosition = drawPositionTable[0]
+func _draw(): #called every time the screen is rendered
+	if radius == 0: #If the ball doesn't exist or is too small to render noticeably
+		radius = rng.randf_range(25.0, 100.0) #generate a ball large enough to render 
+		$Sprite.modulate = Color(rng.randf(),rng.randf(),rng.randf()) #random color generation
+		originalPosition = position #set the origin position of the ball
+		
 	
-	#var shadowPositionTable = WorldToScreen.wp_to_sp(position,0)
-	var shadowPositionTable = WorldToScreen.wp_to_sp(originalPosition,0)
-	if shadowInstance == null:
+	drawPositionTable = WorldToScreen.wp_to_sp(originalPosition,z) #convert ball coordinates to local axis
+	drawPosition = drawPositionTable[0] #prep variables for drawing
+	
+	
+	var shadowPositionTable = WorldToScreen.wp_to_sp(originalPosition,0)#draw the shadows at 0 depth (*ground)
+	if shadowInstance == null: #generate shadow
 		shadowInstance = shadow.instance()
 		get_node("/root/Node2D/").add_child(shadowInstance)
 	
-	#add_child(get_node("/root/Node2D/"))
-	#add_child(shadowInstance)
 	
-	#get_node("..").add_child(shadowInstance)
+	position = drawPosition #yeah
 	
-	#shadowInstance.draw_func(shadowPosition,radius,Color.dimgray)
-	#shadowInstance.pos = shadowPosition
-#	shadowInstance.radius = radius
-#	shadowInstance.color = Color.dimgray
-	position = drawPosition
 	
-	#print(position)
-	var realRadius = (radius/32) #* drawPositionTable[2]
-	var cam = getCurrentCamera2D()
-	var cameraScript = CameraScript
-	#
+	var realRadius = (radius/32) #divide the radius by resolution of sprite because i hate game development
+	var cam = getCurrentCamera2D() #see getCurrentCamera2D
+	var cameraScript = CameraScript#memory leak moment
+	
 	$Sprite.scale=Vector2(realRadius,realRadius)
-	var realRadiusShadow=(radius/32)# * (shadowPositionTable[2])
-	shadowInstance.scale = Vector2(1*realRadiusShadow,realRadiusShadow*cameraScript.yscale)
-	z_index = position.y
-	shadowInstance.z_index = -4095#max is -4096
-	shadowInstance.position = Vector2(shadowPositionTable[0].x,shadowPositionTable[0].y+z)
+	var realRadiusShadow=(radius/32) #no way
+	shadowInstance.scale = Vector2(1*realRadiusShadow,realRadiusShadow*cameraScript.yscale) #flattens the shadows to provide illusion of depth or something like that
+	z_index = drawPositionTable[1] #set the draw order based on the distance from the camera on the z axis like in a 3d game
+	shadowInstance.z_index = -4095#max is -4096, left some room for you :heart:
+	shadowInstance.position = Vector2(shadowPositionTable[0].x,shadowPositionTable[0].y)#+z) #this just puts the shadow in the right spot
 	
-	#draw_circle(drawPosition,radius,Color(rng.randf(),rng.randf(),rng.randf()))
+	
 func _process(delta):
 	update()
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
